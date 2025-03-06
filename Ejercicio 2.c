e9c23869c4fd396dff009ac87db6924b1349fc71
1Manonimus <mangoancestral@gmail.com>
2025-03-06 14:16:50 -0600

    Create Ejercicio 2.c

---
+++ Ejercicio 2.c
@@ -0,0 +1,19 @@
+//López Gutiérrez María Fernanda
+//ejercicio 1
+#include <stdio.h>
+
+int main()
+{
+    int a=3;
+    int b=2;
+ if (a>b)
+ {
+     printf ("a es mayor\n");
+     
+ }
+ else
+ {
+     printf ("b es mayor o igual a\n");
+ }
+    return 0;
+}
