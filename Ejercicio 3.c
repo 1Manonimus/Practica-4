6222d033e7b7202f1080982983d2c0f387df515c
1Manonimus <mangoancestral@gmail.com>
2025-03-06 14:43:01 -0600

    Create Ejercicio 3.c

---
+++ Ejercicio 3.c
@@ -0,0 +1,22 @@
+//López Gutiérrez María Fernanda
+//ejercicio 1
+#include <stdio.h>
+
+int main()
+{
+int res = 0
+int i;
+for(i=0; i < 11; i++)
+
+{
+    res=res + i;
+}
+printf("solución 1 - el número es: %d\n", res);
+
+res =0
+for(i=1 ; <= 10; i++)
+{
+    res=res + i;
+}
+printf("solución 2 - El número es; %d\n" , res)
+    return 0;
